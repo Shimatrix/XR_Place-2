19f6856af420d958719b636a36160e2886c2e08c
Andrew Abramov <andrabra@yandex.ru>
2025-05-02 18:33:55 +0500

    fix: поправил пути к моковым картинкам

--- src/assets/mockProjectCards/index.ts
+++ src/assets/mockProjectCards/index.ts
@@ -9,12 +9,12 @@ export const mockProjectsCards = [
     id: 2,
     title: 'mira',
     description: 'real estate agency',
-    img: '../../../public/mockImg/projectCards/mira.png',
+    img: '/mockImg/projectCards/mira.png',
   },
   {
     id: 3,
     title: 'mira',
     description: 'real estate agency',
-    img: '../../../public/mockImg/projectCards/mira.png',
+    img: '/public/mockImg/projectCards/mira.png',
   },
 ];
