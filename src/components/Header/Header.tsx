74d3dfbd3018f9e14f76fca4058a96f68175f5ea
Andrew Abramov <andrabra@yandex.ru>
2025-05-06 23:24:56 +0500

    feat: сделал адаптив для блока проектов, добавил базовый компонент кнопки

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import styles from "./Header.module.scss";
-import vector from "/src/images/vector.svg";
-import ellips from "/src/images/ellips_ 497.svg";
+import React from 'react';
+import styles from './Header.module.scss';
+import vector from '/src/images/vector.svg';
+import ellips from '/src/images/ellips_ 497.svg';
 
 const Header = () => {
   const menuItems = [
-    { id: 1, title: "О КОМПАНИИ", link: "#about" },
-    { id: 2, title: "КАК ЭТО РАБОТАЕТ", link: "#how-it-works" },
-    { id: 3, title: "ВОЗМОЖНОСТИ", link: "#features" },
+    { id: 1, title: 'О КОМПАНИИ', link: '#about' },
+    { id: 2, title: 'КАК ЭТО РАБОТАЕТ', link: '#how-it-works' },
+    { id: 3, title: 'ВОЗМОЖНОСТИ', link: '#features' },
   ];
 
   return (
@@ -25,7 +25,7 @@ const Header = () => {
               <React.Fragment key={item.id}>
                 {index > 0 && (
                   <li className={styles.menu_divider}>
-                    <div className={styles.divider}></div>
+                    <div className={styles.divider} />
                   </li>
                 )}
                 <li>
@@ -47,7 +47,7 @@ const Header = () => {
             {/* Переключатель языка */}
             <div className={styles.language_switcher}>
               <span className={styles.language_inactive}>EN</span>
-              <div className={styles.language_divider}></div>
+              <div className={styles.language_divider} />
               <span className={styles.language_active}>RU</span>
             </div>
           </div>
