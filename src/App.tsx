de153f82c90093a8235da023da5d7d73ea5143e0
Evgenii Ansel <i@e-ansel.ru>
2025-05-07 17:51:48 +0300

    Merge App.tsx 'develop' into XRP-6-philosophy-block

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 import './App.css';
 import { mockProjectsCards } from './assets/mockProjectCards/index';
 import { ProjectSlider } from './features/Projects/Projects';
+import { useEffect, useState } from 'react';
+import Loader from './components/Loader/Loader';
+import Header from './components/Header/Header';
+import { BlockLabel } from './components/BlockLabel/BlockLabel';
 import BlockClients from './features/BlockClients/BlockClients';
+import Cover from './components/Cover/Cover';
 import Philosophy from '@/features/Philosophy/Philosophy';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Philosophy />
-      <BlockClients />
-      <ProjectSlider projects={mockProjectsCards} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <Cover />
+          <Philosophy />
+          <BlockClients />
+          <ProjectSlider projects={mockProjectsCards} />
+        </>
+      )}
     </>
   );
 }
