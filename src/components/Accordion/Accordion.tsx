8120803967eac4fa02e629fabb98d19dcffdc65f
Eellia <Eelliaoo@yandex.ru>
2025-05-14 14:05:45 +0300

    анимация для FAQ

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -27,49 +27,49 @@ export const Accordion = ({ items }: AccordionProps) => {
         const isOpen = openIndices.includes(index);
 
         return (
-          <div key={index} className={styles.accordionItem}>
-            <div className={styles.accordionQuestion} onClick={() => toggle(index)}>
-              <span>{item.question}</span>
-              <div className={`${styles.plusIcon}`}>
-                <svg
-                  // width="50"
-                  // height="50"
-                  viewBox="0 0 50 50"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g clipPath="url(#clip0_1817_319)">
-                    <circle cx="25" cy="25" r="24.5" stroke="#5E5E5E" strokeOpacity="0.9" />
+          <div
+            key={index}
+            className={`${styles.accordionItem} ${isOpen ? styles.open : ''}`}
+            onClick={() => toggle(index)}
+          >
+            <div className={styles.textWrapper}>
+              <div className={styles.accordionQuestion}>
+                <span className={styles.questionText}>{item.question}</span>
+              </div>
+              <div className={styles.accordionAnswer}>{item.answer}</div>
+            </div>
+            <div className={`${styles.plusIcon}`}>
+              <svg viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clipPath="url(#clip0_1817_319)">
+                  <circle cx="25" cy="25" r="24.5" stroke="#5E5E5E" strokeOpacity="0.9" />
+                  <rect
+                    x="11.6641"
+                    y="23.334"
+                    width="26.6667"
+                    height="2"
+                    transform="rotate(0.170432 11.6641 23.334)"
+                    fill="#5E5E5E"
+                    fillOpacity="0.8"
+                  />
+                  {!isOpen && (
                     <rect
-                      x="11.6641"
-                      y="23.334"
+                      x="23.9941"
+                      y="37.7061"
                       width="26.6667"
                       height="2"
-                      transform="rotate(0.170432 11.6641 23.334)"
+                      transform="rotate(-90 23.9941 37.7061)"
                       fill="#5E5E5E"
                       fillOpacity="0.8"
                     />
-                    {!isOpen && (
-                      <rect
-                        x="23.9941"
-                        y="37.7061"
-                        width="26.6667"
-                        height="2"
-                        transform="rotate(-90 23.9941 37.7061)"
-                        fill="#5E5E5E"
-                        fillOpacity="0.8"
-                      />
-                    )}
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_1817_319">
-                      <rect width="50" height="50" fill="white" />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </div>
+                  )}
+                </g>
+                <defs>
+                  <clipPath id="clip0_1817_319">
+                    <rect width="50" height="50" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
             </div>
-            {isOpen && <div className={styles.accordionAnswer}>{item.answer}</div>}
           </div>
         );
       })}
